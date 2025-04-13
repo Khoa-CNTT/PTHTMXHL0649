6129f8d18bc8555ae7610d6b57a399b90b842b19
dhtuan <hatuan1423@gmail.com>
2025-04-13 12:47:00 +0700

    feat: group management

--- admin_fe/src/pages/Managements/GroupManagement.tsx
+++ admin_fe/src/pages/Managements/GroupManagement.tsx
@@ -2,6 +2,7 @@ import PageBreadcrumb from "../../components/common/PageBreadCrumb";
 import ComponentCard from "../../components/common/ComponentCard";
 import PageMeta from "../../components/common/PageMeta";
 import { APP_NAME } from "../../utils";
+import GroupTable from "@/components/tables/BasicTables/GroupTable";
 
 export default function GroupManagement() {
   return (
@@ -10,7 +11,7 @@ export default function GroupManagement() {
       <PageBreadcrumb pageTitle="Basic Tables" />
       <div className="space-y-6">
         <ComponentCard title="Basic Table 1">
-          <div></div>
+          <GroupTable />
         </ComponentCard>
       </div>
     </>
